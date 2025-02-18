378a33e53a1f0783f1ae338e8df37515563ae7d4
michal-gmail <mkovacikm@gmail.com>
2025-02-18 22:18:48 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,23 @@
-const CACHE_NAME = "pizza-cache-v1";
-const FILES_TO_CACHE = [
-    "index.html",
-    "manifest.json"
-];
-
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
+  event.waitUntil(
+    caches.open("pwa-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/icon-192.png",
+        "/icon-512.png",
+        "/manifest.json",
+        "/style.css",
+        "/script.js"
+      ]);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
