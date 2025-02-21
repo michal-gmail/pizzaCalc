105d3f493eac40ed3b28f24d8072b32f94f22b5f
michal-gmail <mkovacikm@gmail.com>
2025-02-21 17:15:56 +0100

    Add files via upload

---
+++ service-worker2.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'pizzaCalc-app-v2'; // Zmeň číslo verzie
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("pwa-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/icon-192.png",
+        "/icon-512.png",
+        "/manifest.json",
+        "/style.css",
+        "/script.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
